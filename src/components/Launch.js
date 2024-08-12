fa76c26307f8f30fc0e1816fd72b2bcea0aae159
VijayaraghavanPS <56378948+VijayaraghavanPS@users.noreply.github.com>
2024-08-12 14:56:18 +0400

    Updating scope

--- src/components/Launch.js
+++ src/components/Launch.js
@@ -6,7 +6,7 @@ const Launch = () => {
   useEffect(() => {
     SMART.authorize({
       clientId: "b90e1c6f-329e-4a39-b4e3-638c93acbf58",
-      scope: "launch openid fhirUser patient/*.read patient/MedicationRequest.read",
+      scope: "launch openid fhirUser patient/*.read patient/MedicationRequest.read patient/*.search",
       redirectUri: "http://localhost:8080/",
       //pkceMode:true,
     });
